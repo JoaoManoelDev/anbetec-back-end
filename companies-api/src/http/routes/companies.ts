6b74aaa27f9bf21ff0ea6703830cb5ff938a19f4
João Manoel <joaomanoeldev@gmail.com>
2024-05-17 20:25:06 -0300

    feat: create route to delete a company

--- companies-api/src/http/routes/companies.ts
+++ companies-api/src/http/routes/companies.ts
@@ -3,13 +3,16 @@ import { Router } from "express"
 import { CreateCompanyController } from "@/http/controllers/companies/create"
 import { GetCompaniesController } from "@/http/controllers/companies/find"
 import { UpdateCompanyController } from "@/http/controllers/companies/update"
+import { DeleteCompanyController } from "@/http/controllers/companies/delete"
 
 export const companiesRoutes = Router()
 
 const createCompanyController = new CreateCompanyController()
 const getCompaniesController = new GetCompaniesController()
 const updateCompanyController = new UpdateCompanyController()
+const deleteCompanyController = new DeleteCompanyController()
 
 companiesRoutes.get("/", getCompaniesController.handler)
 companiesRoutes.post("/", createCompanyController.handler)
 companiesRoutes.put("/:id", updateCompanyController.handler)
+companiesRoutes.delete("/:id", deleteCompanyController.handler)
