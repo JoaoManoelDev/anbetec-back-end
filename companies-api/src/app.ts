daffcfb24d898f04c198a8ba530a4d49c0368a63
João Manoel <joaomanoeldev@gmail.com>
2024-05-17 16:58:12 -0300

    feat: add routes and erros to app use

--- companies-api/src/app.ts
+++ companies-api/src/app.ts
@@ -1,5 +1,14 @@
 import express from "express"
 
+import { errorHandler } from "@/middlewares/error"
+import { routes } from "@/http/routes"
+
 const app = express()
+
+app.use(express.json())
+
+app.use(routes)
+
+app.use(errorHandler)
 
 export { app }
