a501accfb91ef931c86def0381581e22fa8ccce5
João Manoel <joaomanoeldev@gmail.com>
2024-05-17 19:30:41 -0300

    feat: create route to update company

--- companies-api/src/http/routes/companies.ts
+++ companies-api/src/http/routes/companies.ts
@@ -2,11 +2,14 @@ import { Router } from "express"
 
 import { CreateCompanyController } from "@/http/controllers/companies/create"
 import { GetCompaniesController } from "@/http/controllers/companies/find"
+import { UpdateCompanyController } from "@/http/controllers/companies/update"
 
 export const companiesRoutes = Router()
 
 const createCompanyController = new CreateCompanyController()
 const getCompaniesController = new GetCompaniesController()
+const updateCompanyController = new UpdateCompanyController()
 
 companiesRoutes.get("/", getCompaniesController.handler)
 companiesRoutes.post("/", createCompanyController.handler)
+companiesRoutes.put("/:id", updateCompanyController.handler)
