5f2e2058ba1f502c70bd84e0d3e9c93ceeb26f43
João Manoel <joaomanoeldev@gmail.com>
2024-05-17 17:55:06 -0300

    feat: create route to get companies

--- companies-api/src/http/routes/companies.ts
+++ companies-api/src/http/routes/companies.ts
@@ -1,8 +1,12 @@
 import { Router } from "express"
+
 import { CreateCompanyController } from "@/http/controllers/companies/create"
+import { GetCompaniesController } from "@/http/controllers/companies/find"
 
 export const companiesRoutes = Router()
 
 const createCompanyController = new CreateCompanyController()
+const getCompaniesController = new GetCompaniesController()
 
+companiesRoutes.get("/", getCompaniesController.handler)
 companiesRoutes.post("/", createCompanyController.handler)
