832088d9ca52dd55a285ab3915a69388494442cf
João Manoel <joaomanoeldev@gmail.com>
2024-05-18 19:26:28 -0300

    feat: use CompanyUpdateInput in save

--- companies-api/src/repositories/companies-repository.ts
+++ companies-api/src/repositories/companies-repository.ts
@@ -1,10 +1,14 @@
-import { Company, CompanyInput } from "@/repositories/dtos/company"
+import {
+  Company,
+  CompanyInput,
+  CompanyUpdateInput
+} from "@/repositories/dtos/company"
 
 export interface CompaniesRepository {
   create(company: CompanyInput): Promise<Company>
   findMany(): Promise<Company[] | []>
   findById(companyId: string): Promise<Company | null>
   findByCNPJ(companyCNPJ: string): Promise<Company | null>
-  save(company: Company): Promise<Company>
+  save(company: CompanyUpdateInput): Promise<Company>
   delete(companyId: string): Promise<void>
 }
