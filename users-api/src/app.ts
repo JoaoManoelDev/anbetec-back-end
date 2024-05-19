ebbf47c95f0d3f0f2b97e412e87a16d29722b253
João Manoel <joaomanoeldev@gmail.com>
2024-05-19 01:37:58 -0300

    chore: add cors

--- users-api/src/app.ts
+++ users-api/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express"
+import cors from "cors"
+
 import { routes } from "@/http/routes"
 import { errorHandler } from "@/middlewares/error"
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
