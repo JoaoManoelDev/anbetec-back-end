7d38c179ca5bc11253281dfe7d73ee82c59c5b71
João Manoel <joaomanoeldev@gmail.com>
2024-05-18 21:43:21 -0300

    test: create test to should not be able to create a user with wrong email and cpf

--- users-api/src/use-cases/create-user.spec.ts
+++ users-api/src/use-cases/create-user.spec.ts
@@ -3,6 +3,8 @@ import { beforeEach, describe, expect, it } from "vitest"
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository"
 import { CreateUserUseCase } from "@/use-cases/create-user"
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error"
+import { EmailAlreadyExistsError } from "@/use-cases/errors/email-already-exists-error"
+import { CpfAlreadyExistsError } from "@/use-cases/errors/cpf-already-exists-error"
 
 let usersRepository: InMemoryUsersRepository
 let sut: CreateUserUseCase
@@ -20,7 +22,6 @@ describe("Create User Use Case", () => {
       cpf: "123456788998",
       password: "123456789",
       phone: "999999999",
-      company_id: ""
     })
 
     expect(user.name).toEqual("John Doe")
@@ -34,7 +35,6 @@ describe("Create User Use Case", () => {
       cpf: "123456788998",
       password: "123456789",
       phone: "999999999",
-      company_id: ""
     })
 
     await expect(() => sut.execute({
@@ -43,8 +43,64 @@ describe("Create User Use Case", () => {
       cpf: "123456788998",
       password: "123456789",
       phone: "999999999",
-      company_id: ""
     })).rejects.toBeInstanceOf(UserAlreadyExistsError)
+   
+  })
+
+  it("should not be able to create user with wrong name", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      cpf: "123456788998",
+      password: "123456789",
+      phone: "999999999",
+    })
+
+    await expect(() => sut.execute({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      cpf: "123456788998",
+      password: "123456789",
+      phone: "999999999",
+    })).rejects.toBeInstanceOf(UserAlreadyExistsError)
+   
+  })
+
+  it("should not be able to create user with wrong email", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      cpf: "123456788998",
+      password: "123456789",
+      phone: "999999999",
+    })
+
+    await expect(() => sut.execute({
+      name: "John Doe 2",
+      email: "johndoe@email.com",
+      cpf: "123456788998",
+      password: "123456789",
+      phone: "999999999",
+    })).rejects.toBeInstanceOf(EmailAlreadyExistsError)
+   
+  })
+
+  it("should not be able to create user with wrong cpf", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      cpf: "123456788998",
+      password: "123456789",
+      phone: "999999999",
+    })
+
+    await expect(() => sut.execute({
+      name: "John Doe 2",
+      email: "johndoe2@email.com",
+      cpf: "123456788998",
+      password: "123456789",
+      phone: "999999999",
+    })).rejects.toBeInstanceOf(CpfAlreadyExistsError)
    
   })
 })
