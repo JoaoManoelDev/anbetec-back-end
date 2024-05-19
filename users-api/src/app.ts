b3feb0c13be41245cd63994629e24a17cb775f8e
João Manoel <joaomanoeldev@gmail.com>
2024-05-18 21:50:18 -0300

    feat: add app use dependencies

--- users-api/src/app.ts
+++ users-api/src/app.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import { routes } from "@/http/routes"
+import { errorHandler } from "@/middlewares/error"
 
 const app = express()
 
 app.use(express.json())
+
+app.use(routes)
+
+app.use(errorHandler)
 
 export { app }
