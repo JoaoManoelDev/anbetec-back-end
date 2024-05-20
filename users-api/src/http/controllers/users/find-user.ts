f6f6b15bd3d1a8aa565c085c9429530e3b544552
João Manoel <joaomanoeldev@gmail.com>
2024-05-20 00:23:39 -0300

    feat: use ResourceNotFoundError

--- users-api/src/http/controllers/users/find-user.ts
+++ users-api/src/http/controllers/users/find-user.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 
 import { makeGetUserUseCase } from "@/use-cases/factories/make-get-user-use-case"
+import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error"
 
 export class GetUserController {
   async handler(request: Request, response: Response, next: NextFunction) {
@@ -14,6 +15,9 @@ export class GetUserController {
       return response.status(200).json(user)
 
     } catch (error) {
+      if (error instanceof ResourceNotFoundError) {
+        return response.status(409).json({ message: error.message })
+      }
       return next(error)
     }
   }
