7c1ef26520b93c6fa40fef082ff7b3dcc6313b53
João Manoel <joaomanoeldev@gmail.com>
2024-05-20 12:15:04 -0300

    refactor: remove comparePassword function not use

--- users-api/src/lib/hash.ts
+++ users-api/src/lib/hash.ts
@@ -1,13 +1,8 @@
-import { compare, hash } from "bcryptjs"
+import { hash } from "bcryptjs"
 
 interface PasswordHashProps {
   password: string
   salt?: string | number
-}
-
-interface ComparePasswordProps {
-  password: string
-  hash: string
 }
 
 export const passwordHash = async ({
@@ -15,13 +10,4 @@ export const passwordHash = async ({
   salt = 6
 }: PasswordHashProps) => {
   return await hash(password, salt)
-}
-
-export const comparePassword = async ({
-  password,
-  hash
-}: ComparePasswordProps) => {
-  const passwordCompared = await compare(password, hash)
-
-  return passwordCompared
 }
