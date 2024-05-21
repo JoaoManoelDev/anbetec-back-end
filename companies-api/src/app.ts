8a1fb1ddb837d16f81df5106212740ded6868767
João Manoel <joaomanoeldev@gmail.com>
2024-05-20 23:07:59 -0300

    chore: setup cors

--- companies-api/src/app.ts
+++ companies-api/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import cors from "cors"
 
 import { errorHandler } from "@/middlewares/error"
 import { routes } from "@/http/routes"
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
